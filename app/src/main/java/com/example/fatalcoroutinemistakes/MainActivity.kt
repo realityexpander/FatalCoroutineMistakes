4ec8fdcec62fce84ac4af75c2cf43d9375247420
Chris Athanas <realityexpander@gmail.com>
2022-05-27 01:11:01 -0500

    Add output to compose

--- app/src/main/java/com/example/fatalcoroutinemistakes/MainActivity.kt
+++ app/src/main/java/com/example/fatalcoroutinemistakes/MainActivity.kt
@@ -4,45 +4,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.example.fatalcoroutinemistakes.ui.theme.FatalCoroutineMistakesTheme
 import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val userIds = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        var userFirstNames = listOf<String>()
+        lifecycleScope.launch {
+            userFirstNames = getUserFirstNames(userIds)
+            println("userFirstnames: $userFirstNames")
+        }
+
         setContent {
+            var coroutineScope = rememberCoroutineScope()
+
             FatalCoroutineMistakesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    var names by remember { mutableStateOf(listOf<String>()) }
+
+                    LaunchedEffect(Unit) {
+                        names = getUserFirstNames(userIds)
+                    }
+
+                    Greeting(names.toString())
+
                 }
             }
         }
     }
 }
 
-// Mistake #1
-suspend fun getUserFirstNames(userIds: List<String>): List<String> {
+
+// from Philip Lackner - 5 Fatal Coroutine Mistakes Nobody Tells You About
+// https://www.youtube.com/watch?v=cr5xLjPC4-0
+
+// Mistake #1 - dont need to do things sequentially here
+suspend fun getUserFirstNames(userIds: List<Int>): List<String> {
+    // problem - will be executed in serial
     val firstNames = mutableListOf<String>()
-    for (userId in userIds) {
-        firstNames.add(getFirstName(userId))
+//    for (userId in userIds) {
+//        firstNames.add(getFirstName(userId)) // problem - will be executed in serial
+//    }
+
+    // solution - will be executed in parallel
+    val firstNames2 = mutableListOf<Deferred<String>>()
+    coroutineScope {
+        for (userId in userIds) {
+            firstNames2.add(async { getFirstName(userId) }) // solution - will be setup to be executed in parallel
+        }
     }
 
-    return firstNames
+    //return firstNames
+    return firstNames2.awaitAll() // execute all the async calls in parallel, and wait until they are all done
 }
 
-suspend fun getFirstName(userId: String): String {
-    return "John"
+suspend fun getFirstName(userId: Int): String {
+    delay(500)
+    return "John $userId"
 }
+
+// Mistake #2
+
+
 
 @Composable
 fun Greeting(name: String) {
